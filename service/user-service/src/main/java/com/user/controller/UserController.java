a76fc8f1c596c0f56b766c8f725fa0b9f0264aed
chuan.qu@ebuilder.com.cn <ebuilder123>
2018-06-13 19:21:47 +0800

    创建了一个客户端

---
+++ service/user-service/src/main/java/com/user/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.user.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lvchao
+ * @date 2018/6/13
+ */
+@RestController
+@RequestMapping(value = "user")
+public class UserController {
+
+    private  final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @GetMapping(value = "hello")
+    public String hello(){
+        ServiceInstance serviceInstance = client.getLocalServiceInstance();
+        logger.info("/user/hello host:{} service_id:{}",serviceInstance.getHost(),serviceInstance.getServiceId());
+        return "hello";
+    }
+}
