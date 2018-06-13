9127b37560a6fdeaf4c2adfc14baf7d2634cded2
chuan.qu@ebuilder.com.cn <ebuilder123>
2018-06-13 15:15:30 +0800

    spring boot actuator 监控配置

--- demo/src/main/java/com/demo/DemoApplication.java
+++ demo/src/main/java/com/demo/DemoApplication.java
@@ -4,7 +4,9 @@ import com.common.TestUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 @SpringBootApplication
