41e1b4278011c80d76275092465a6253aeaf8d38
chuan.qu@ebuilder.com.cn <ebuilder123>
2018-06-14 15:48:32 +0800

    配置eureka集群高可用的demo

--- service/user-service/src/test/java/com/user/UserServiceApplicationTests.java
+++ service/user-service/src/test/java/com/user/UserServiceApplicationTests.java
@@ -1,16 +1,39 @@
 package com.user;
 
+import com.user.controller.UserController;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class UserServiceApplicationTests {
 
+    private MockMvc mockMvc;
+
+    @Before
+    public void setMockMvc() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+    }
+
     @Test
-    public void contextLoads() {
+    public void hello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.equalToIgnoringCase("hello")));
+
+
     }
 
 }
